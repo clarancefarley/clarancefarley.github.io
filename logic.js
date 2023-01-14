3bac6902eb98329690c4beaa9db3ffee34db159d
clarancefarley <clarancefarely@gmail.com>
2023-01-14 16:02:21 -0600

    update share feature

--- logic.js
+++ logic.js
@@ -423,7 +423,7 @@ function finalPage() {
 
     shareBtn.addEventListener('click', function() {
       navigator.share({
-          title: 'My Cool App',
+          title: 'UX Quiz',
           text: 'Check out this cool app I made!',
           url: 'https://clarancefarley.github.io/'
       })
